67e8bb000ec6b6c8cdbf313728086000cdb89498
talos <talos@digitalforge.cc.cc>
2017-02-13 02:42:45 +0700

    Editing readme file. Configuring database. Cleaning bat file (not a Windows OS)

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=yii2bank',
+    'username' => 'bankadmin',
+    'password' => '0Filfn6BcjvvgHy6',
     'charset' => 'utf8',
 ];
