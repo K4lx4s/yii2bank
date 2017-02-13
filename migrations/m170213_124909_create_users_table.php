9143a8a8229270edf726ba0855d8444142882e9d
talos <talos@digitalforge.cc.cc>
2017-02-14 03:08:10 +0700

    adding population of users table

--- migrations/m170213_124909_create_users_table.php
+++ migrations/m170213_124909_create_users_table.php
@@ -21,6 +21,19 @@ class m170213_124909_create_users_table extends Migration
             'address' => $this->text(100)->notNull(),
             'current_balance' => $this->float(10,2)->notNull()
         ]);
+
+        /* populate the database with 10 existing users users*/
+        defined('EXISTING_USERS') or define('EXISTING_USERS', 10);
+        for ($cur_user=1;$cur_user<EXISTING_USERS+1;$cur_user++){
+        $this->insert('users', [
+            'username' => 'client'."$cur_user",
+            'password' => 'strong*password*of*client*'."$cur_user",
+            'name' => 'firstname'."$cur_user".' lastname'."$cur_user",
+            'email' => 'client'."$cur_user".'@domain'."$cur_user".'.com',
+            'address' => 'address of client'."$cur_user",
+            'current_balance' => 10000.00
+        ]);
+        }        
     }
 
     /**
