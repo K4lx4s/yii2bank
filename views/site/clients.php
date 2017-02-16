e4d0ff65dcf0ce0e221a49ad4945a5df791b8571
talos <talos@digitalforge.cc.cc>
2017-02-16 12:03:01 +0700

    completing clientlist page

--- views/site/clients.php
+++ views/site/clients.php
@@ -4,15 +4,58 @@
 
 use yii\helpers\Html;
 
-$this->title = 'About';
+$this->title = 'Our clients';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-about">
+<div class="site-clients">
     <h1><?= Html::encode($this->title) ?></h1>
+<?php
+$clients = (new \yii\db\Query())
+    ->select(['name','email','address','current_balance'])
+    ->from('users')                           
+    ->all();
+$client_numbers=count($clients);
+echo '<div class="col-lg-3">';
+echo '<ul  class="list-group">';
+echo '<li class="list-group-item list-group-item-warning">Name</li>';
+$c=0 ; while ($c<$client_numbers){
+    echo '<li class="list-group-item list-group-item-info">'.$clients[$c]['name'];
+    echo '</li>';
+    $c++;
+}
+echo '</ul>';
+echo '</div>';
+echo '<div class="col-lg-3">';
+echo '<ul  class="list-group">';
+echo '<li class="list-group-item list-group-item-warning">email</li>';
+$c=0 ; while ($c<$client_numbers){
+    echo '<li class="list-group-item list-group-item-info">'.$clients[$c]['email'];
+    echo '</li>';
+    $c++;
+}
+echo '</ul>';
+echo '</div>';
+echo '<div class="col-lg-3">';
+echo '<ul  class="list-group">';
+echo '<li class="list-group-item list-group-item-warning">address</li>';
+$c=0 ; while ($c<$client_numbers){
+    echo '<li class="list-group-item list-group-item-info">'.$clients[$c]['address'];
+    echo '</li>';
+    $c++;
+}
+echo '</ul>';
+echo '</div>';
+echo '<div class="col-lg-3">';
+echo '<ul  class="list-group">';
+echo '<li class="list-group-item list-group-item-warning">balance</li>';
+$c=0 ; while ($c<$client_numbers){
+    echo '<li class="list-group-item list-group-item-info">';
+    print number_format($clients[$c]['current_balance'], 2, '.', ',');
+    echo '</li>';
+    $c++;
+}
+echo '</ul>';
+echo '</div>';
+?>
 
-    <p>
-        This is the About page. You may modify the following file to customize its content:
-    </p>
-
-    <code><?= __FILE__ ?></code>
 </div>
